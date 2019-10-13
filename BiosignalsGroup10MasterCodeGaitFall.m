45bfb2da8097ef86f469c7163fe889c415aebbae
TPENKO <TPENKO@NE136019.utad.utoledo.edu>
2019-10-13 12:23:02 -0400

    Added xlsread command + testing tanner's branch response

--- BiosignalsGroup10MasterCodeGaitFall.m
+++ BiosignalsGroup10MasterCodeGaitFall.m
@@ -1,3 +1,7 @@
 %Biosignals group 10 test.
 % This is the master branch code for gait fall index prediction
 %This is a test change to see if github updates
+%This is me working in Tanner's branch only
+
+
+%[PatientInfoText, PatientInfoNumeric, PatientInfoRaw] = xlsread(GaitFFILE HERE);
