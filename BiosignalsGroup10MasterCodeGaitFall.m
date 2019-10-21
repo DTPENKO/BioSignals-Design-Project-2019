0cdad21e340ccc5d2c42363930051b494d0e888b
TPENKO <TPENKO@NE104711.utad.utoledo.edu>
2019-10-20 20:47:16 -0400

    Added more windowing code

--- BiosignalsGroup10MasterCodeGaitFall.m
+++ BiosignalsGroup10MasterCodeGaitFall.m
@@ -32,15 +32,49 @@
     end
 
 
-% INITIAL PLOTTING AND WINDOWING
-% Note this may need to be limited to one or two files if datasets chosen
-% is large. 
+% INITIAL PLOTTING AND WINDOWING *** This may need to be limited
 
 fs = 100; % Samples/second
 WindowSize = 60; % Seconds of data in the window
 PercentOverlap = 0.5; % Window over lap from previous window (percentage of samples)
 SamplesPerWindow = WindowSize * fs; % Number of samples in a window
 WindowIncrementSwitch = (1-PercentOverlap) * SamplesPerWindow; % Number of samples in an incremented window
+WindowIncrementTime = (((1-PercentOverlap)*SamplesPerWindow)/fs); % Time interval in a window increment used for number of samples
+StartIndex = 1 % Sample to start with
+EndIndex = SamplesPerWindow;
+WindowCounter = 0; % Number goes up each window
+
+Sample2Process = XXXX % Select sample that will be windowed or lay multiple sample on the same thing
+% Call plotATM to do all of them?
+
+while(endIndex<=length(Sample2Process))
+   
+    %Assign EEG data for current window to currEEGData
+    currEEGData = convertedEEGData(startIndex:endIndex,eegChannel);
+    
+    %Plot Current EEG Data in Specified Window
+    hold on
+    plot(currEEGData);
+    xlabel('Sample Number (n)');
+    ylabel('Voltage (microVolts)');
+    %Set ylimit for plotting visualization
+    ylim([-200,200]);
+    
+    %Calculate mean voltage for current window
+    currWindMean = mean(currEEGData);
+    
+    %Plot Mean in Plot Title 
+    title(['The mean voltage of the current EEG window is: ',num2str(currWindMean)]);
+    
+    %Increment EEG Data to next window for analysis and plotting
+    startIndex = startIndex+windIncrement;
+    endIndex = endIndex+windIncrement;
+   
+    %Wait for key to be pressed to increment through next window
+    waitforbuttonpress;
+     
+end
+
 
 
 % PROCESSING FUNCTIONS BELOW
